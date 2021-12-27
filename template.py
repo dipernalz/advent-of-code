99b83701ecf941e1c2c253ac331ad3b8da28c751
dipernalz <lzdiperna@gmail.com>
2021-12-27 16:17:53 -0500

    Added python template

---
+++ template.py
@@ -0,0 +1,8 @@
+from collections import Counter, defaultdict, deque
+from functools import cache
+from heapq import *
+from itertools import combinations, permutations, product
+import math
+import re
+
+inpt = open('input.txt', 'r').read().strip().split('\n')
