d92ba5e82b3941503665ec9a64f59b0731be65e3
dipernalz <lzdiperna@gmail.com>
2022-01-07 19:59:41 -0500

    2018 day 5

---
+++ 2018/05/rust/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+fn main() {
+    let inpt = include_str!("input.txt").trim();
+    let mut polymer = Vec::new();
+    for c in inpt.chars() {
+        polymer.push(c);
+    }
+
+    let mut adjacent = HashMap::new();
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for c in alphabet.chars() {
+        adjacent.insert(c, c.to_ascii_uppercase());
+        adjacent.insert(c.to_ascii_uppercase(), c);
+    }
+
+    // Part 1
+    let mut temp = polymer.clone();
+    println!("{}", react(&mut temp, &adjacent));
+
+    // Part 2
+    let mut min_length = inpt.len() as u32;
+    for c in alphabet.chars() {
+        let mut temp = polymer.clone();
+        temp.retain(|&a| a != c && a != c.to_ascii_uppercase());
+        min_length = std::cmp::min(min_length, react(&mut temp, &adjacent));
+    }
+    println!("{}", min_length);
+}
+
+fn react(polymer: &mut Vec<char>, adjacent: &HashMap<char, char>) -> u32 {
+    let (mut i, mut j, mut length) = (0, 1, polymer.len() as u32);
+    let mut stack = Vec::new();
+    while j < polymer.len() {
+        if polymer[j] == adjacent[&polymer[i]] {
+            length -= 2;
+            polymer[i] = '\0';
+            polymer[j] = '\0';
+            match stack.pop() {
+                Some(k) => {
+                    i = k;
+                }
+                None => {
+                    i = j + 1;
+                    j += 1;
+                }
+            }
+        } else {
+            stack.push(i);
+            i = j;
+        }
+        j += 1;
+    }
+    return length;
+}
--- template.rs
+++ template.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
+
     let inpt: Vec<&str> =
         include_str!("input.txt").trim().split("\n").collect();
     let inpt: Vec<i32> = include_str!("input.txt")
@@ -9,4 +12,7 @@ fn main() {
         .split("\n")
         .map(|s| s.parse::<i32>().unwrap())
         .collect();
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
